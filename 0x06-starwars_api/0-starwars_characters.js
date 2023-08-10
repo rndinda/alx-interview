1c73b987bd2f4d598d0b94a2a5d96f6dc33da318
rndinda <rndinda73@gmail.com>
2023-08-10 19:06:02 +0300

    Wars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,32 @@
 #!/usr/bin/node
-// using star wars API
 
+const { exit } = require('process');
 const request = require('request');
-const FILMID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const argument = process.argv[2];
 
-// Request  for URL
-const URL_BASE = 'https://swapi-api.hbtn.io/api/films';
+if (argument === undefined) {
+  exit();
+}
 
-function doRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, res, body) {
-      if (!error && res.statusCode === 200) {
-        resolve(JSON.parse(body));
-      } else {
-        reject(error);
-      }
-    });
+request(url + argument, (error, response, body) => {
+  if (!error) {
+    const chars = JSON.parse(body).characters;
+    orderPrinter(chars, 0);
+  } else {
+    console.log(error);
+  }
+});
+
+function orderPrinter (chars, n) {
+  if (n === chars.length) { return; }
+  request(chars[n], (error, response, body) => {
+    if (!error) {
+      const name = JSON.parse(body).name;
+      console.log(name);
+      orderPrinter(chars, n + 1);
+    } else {
+      console.log('error:', error);
+    }
   });
 }
-
-// Usage:
-
-async function main (filmID) {
-  const res = await doRequest(`${URL_BASE}/${filmID}`);
-  for (const e of res.characters) {
-    const pj = await doRequest(e);
-    console.log(pj.name);
-  }
-}
-
-main(FILMID);
